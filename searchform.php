1b5cfd2215f3e678f5bbda7bd9c5b74ef9623ddd
Andrew Paschall <work.andrewpaschall@gmail.com>
2019-11-22 14:17:49 -0500

    Fixed sidebar search form after page search form updates

--- searchform.php
+++ searchform.php
@@ -1,4 +1,12 @@
-<form role="search" method="get" class="search-form" id="searchform" action="<?php echo home_url( '/' ); ?>">
-    <input type="search" value="" name="s" id="s" style="background-image: url('/wp-content/themes/special-lite/assets/img/ic-search-48px.svg'); padding-left: 3em; background-repeat: no-repeat; background-position: 1px 1px;" placeholder="Search" title="Search For:"/>
-    <button type="submit" id="searchsubmit" value="Search" class="button large" style="padding-left:5%; padding-right: 5%;">Search</button>
-</form>
+<?php
+    if ( is_404() or is_search() ) {
+?>
+    <form role="search" method="get" class="search-form" id="searchform" action="<?php echo home_url( '/' ); ?>">
+        <input type="search" value="" name="s" id="s" style="background-image: url('/wp-content/themes/special-lite/assets/img/ic-search-48px.svg'); padding-left: 3em; background-repeat: no-repeat; background-position: 1px 1px;" placeholder="Search" title="Search For:"/>
+        <button type="submit" id="searchsubmit" value="Search" class="button large" style="padding-left:5%; padding-right: 5%;">Search</button>
+    </form>
+<?php } else { ?>
+    <form role="search" method="get" class="search-form" id="searchform" action="<?php echo home_url( '/' ); ?>" style="padding-top: 1em;">
+        <input type="search" value="" name="s" id="s" style="background-image: url('/wp-content/themes/special-lite/assets/img/ic-search-48px.svg'); padding-left: 3em; background-repeat: no-repeat; background-position: 1px 1px;" placeholder="Search" title="Search For:"/>
+    </form>
+<?php } ?>
